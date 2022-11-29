209b04f4a6ba5c7dafaeecbdbcd8d369fb2cd697
tingzhao.ytz <tingzhao.ytz@antgroup.com>
2022-11-29 16:05:46 +0800

    fix: account info show bug

--- dapp-alipay-mini/src/pages/index/index.ts
+++ dapp-alipay-mini/src/pages/index/index.ts
@@ -6,7 +6,7 @@ import {
 import { testContractABI as abi } from "../../contracts/test-contract";
 import { getRandomPhrase } from "../../utils";
 
-const provider = new AlipayMiniProvider({ chainId: ChainIds.OpenLab });
+const provider = new AlipayMiniProvider({ chainId: ChainIds.LabChain });
 const contract = new SolContract({ contractName: "", abi }, provider);
 
 Page({
@@ -20,7 +20,7 @@ Page({
   },
   async handleConnect() {
     try {
-      const account = await provider.requestAccounts();
+      const [ account ] = await provider.requestAccounts();
       this.setData({ account, connectError: "" });
     } catch (e) {
       this.setData({ account: null, connectError: "连接失败，请重试" });
@@ -55,6 +55,20 @@ Page({
         signResult: "",
         signError: `error: ${e.message || "unknown"}`,
       });
+    }
+  },
+
+
+  async onShow() {
+    try {
+      // 在部分 Android 机器上，因为跳转回来无法出发 onAppShow（JSSDK 中会用到），导致无法正常出发 JSSDK 的调用
+      // 该方案为一个临时解决方案，未来 getAndriodAuthorizeResult 方法可能会移除，相关内容联系 A-Pocket 相关同学获取支持
+      const [ account ] = AlipayMiniProvider.getAndriodAuthorizeResult();
+      this.setData({
+        account,
+      });
+    } catch (e) {
+      console.error(e);
     }
   },
 });
